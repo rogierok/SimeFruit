531959bee517aef2e8f15ef80391d608df711a53
Rogier Goossen <92125322+rogierok@users.noreply.github.com>
2023-07-19 23:44:33 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("hello")
+
+sime = document.getElementById("sime")
+txt = document.getElementById("txt")
+
+
+sime.onclick = function(){
+    console.log("hello");
+    sime.style.transform = 'scale(0.005)';
+    txt.innerHTML = "Why did you punch me man... Now i am SOOO far away, now refresh because i am not coming back anymore";
+}
+
+txt.onclick
+
+
+
+
+
+
+txt.innerHTML = "Im floating in space!"
+
+    
+
+
+
+
+    
+
